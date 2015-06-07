79ca2dccb908395b6cb108a8adf393f9451a9483
Aruna Tebel <arunatebel@hotmail.com>
2015-06-07 17:39:35 +0530

    Added Stylesheets and Javascripts Styled the templates Added relationships between categories and items

--- src/StockManagerBundle/Form/CategoryType.php
+++ src/StockManagerBundle/Form/CategoryType.php
@@ -1,17 +1,29 @@
 <?php
+
 namespace StockManagerBundle\Form;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-class CategoryType extends AbstractType{
-    
-    public function buildForm(FormBuilderInterface $builder,array $options) {
- 
-        $builder->add('category_name');
-        $builder->add('category_code');
-        $builder->add('Save','submit');
-       
+
+class CategoryType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
+        $builder->add('category_name', 'text', array(
+            'attr' => array(
+                'placeholder' => 'Category Name',
+            ),
+            'label' => false,
+        ));
+        $builder->add('category_code', 'text', array(
+            'attr' => array(
+                'placeholder' => 'Category Code',
+            ),
+            'label' => false,
+        ));
+        $builder->add('Save', 'submit');
     }
-    
+
     public function getName() {
         return 'category';
     }
